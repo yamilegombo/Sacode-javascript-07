54d846692cc1cd2c9dd96fd3bd444b2889f5822f
Mile Gombo <yamilegombo30@gmail.com>
2024-06-11 20:05:00 +0900

    feat: menambahkan index, script, style

---
+++ script.js
@@ -0,0 +1,13 @@
+document
+// .getElementById('pesan')
+// .querySelector('#pesan')
+//.textContent = 'Selamat Datang!';
+
+var elemenBaru = document.createElement("strong");
+elemenBaru.textContent = "di sacode community!";
+
+document.getElementById("pesan").appendChild("elemenBaru");
+
+ document
+ .querySelector(".selamat")
+.textContent = "Happy Coding!";
